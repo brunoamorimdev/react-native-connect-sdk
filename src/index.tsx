645f11c7532d1909b141795e4e473bd88761eeb1
Bruno Amrim <brunoamorim.dev@gmail.com>
2024-04-21 22:57:19 -0300

    feat: improve JS API for native events & methods

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { NativeModules, Platform, NativeEventEmitter } from 'react-native';
+import type { EmitterSubscription } from 'react-native';
 
 const LINKING_ERROR =
   `The package 'react-native-connect-sdk' doesn't seem to be linked. Make sure: \n\n` +
@@ -13,7 +14,31 @@ const ConnectSdkModule = isTurboModuleEnabled
   ? require('./NativeConnectionManager').default
   : NativeModules.ConnectionManager;
 
-const ConnectSdk = ConnectSdkModule
+export interface ConnectableDevice {
+  ipAddress: string | null;
+  friendlyName: string | null;
+}
+
+export type ConnectsEvents =
+  | 'didFindDevice'
+  | 'didLoseDevice'
+  | 'didFailWithError'
+  | 'didUpdateDevice';
+
+export interface ConnectSDK extends NativeEventEmitter {
+  startDiscovery(): Promise<void>;
+  stopDiscovery(): Promise<void>;
+  connect(ipAddress: string): Promise<string>;
+  getAllDevices(): Promise<ConnectableDevice[]>;
+  removeListeners(): void;
+  addListener(
+    eventType: ConnectsEvents,
+    listener?: (event: any) => void,
+    context?: Object
+  ): EmitterSubscription;
+}
+
+const Module: ConnectSDK = ConnectSdkModule
   ? ConnectSdkModule
   : new Proxy(
       {},
@@ -24,10 +49,9 @@ const ConnectSdk = ConnectSdkModule
       }
     );
 
-export const nativeEvent = new NativeEventEmitter(ConnectSdk);
+const { addListener, removeAllListeners } = new NativeEventEmitter(Module);
 
-export function startDiscovery(): Promise<void> {
-  return ConnectSdk.startDiscovery();
-}
-
-startDiscovery();
+export const ConnectSDK = Object.assign(Module, {
+  addListener,
+  removeAllListeners,
+});
